d8fe5b3c598cf0c195da67740c7a531d172cf537
Dan Putnam <daniel.a.putnam@gmail.com>
2017-09-17 18:11:06 -0500

    final

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+  return [
+    {title: 'Javascript', pages: 22},
+    {title: 'Harry Potter', pages: 214},
+    {title: 'Scary Potter', pages: 22},
+    {title: 'The Adventures of Karaline', pages: 2222}
+  ]
+} 
+
